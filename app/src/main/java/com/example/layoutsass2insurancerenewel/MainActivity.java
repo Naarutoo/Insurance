d4c5dc1fc9cb49fd217640b9beb8c152a9fd5d9e
saurabh09111996 <saurabhsharma708@gmail.com>
2021-07-04 10:55:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/layoutsass2insurancerenewel/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.layoutsass2insurancerenewel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
